597f53ca294e1415c038cc3a36f5fdb11e0cc9f8
jzdwk <jzd_wk@126.com>
2020-06-18 08:58:09 +0800

    modify chainofrepb

--- go-pattern/behavioral/chainofrspb.go
+++ go-pattern/behavioral/chainofrspb.go
@@ -21,59 +21,61 @@ refer to 	https://github.com/pibigstar/go-demo/blob/master/design/chain/chain.go
 //统一的接口定义，handle方法用于处理业务逻辑，next方法引用自身
 type Handler interface {
 	Handle(content string)
+	//关键代码
 	next(handler Handler, content string)
 }
 
 // 广告过滤
 type AdHandler struct {
-	handler Handler
+	nextHandler Handler
 }
 
 func (ad *AdHandler) Handle(content string) {
 	fmt.Println("执行广告过滤。。。")
 	newContent := strings.Replace(content, "广告", "**", 1)
 	fmt.Println(newContent)
-	ad.next(ad.handler, newContent)
+	//关键代码
+	ad.next(ad.nextHandler, newContent)
 }
 
 func (ad *AdHandler) next(handler Handler, content string) {
-	if ad.handler != nil {
-		ad.handler.Handle(content)
+	if handler != nil {
+		handler.Handle(content)
 	}
 }
 
 // 涉黄过滤
 type YellowHandler struct {
-	handler Handler
+	nextHandler Handler
 }
 
 func (yellow *YellowHandler) Handle(content string) {
 	fmt.Println("执行涉黄过滤。。。")
 	newContent := strings.Replace(content, "涉黄", "**", 1)
 	fmt.Println(newContent)
-	yellow.next(yellow.handler, newContent)
+	yellow.next(yellow.nextHandler, newContent)
 }
 
 func (yellow *YellowHandler) next(handler Handler, content string) {
-	if yellow.handler != nil {
-		yellow.handler.Handle(content)
+	if handler != nil {
+		handler.Handle(content)
 	}
 }
 
 // 敏感词过滤
 type SensitiveHandler struct {
-	handler Handler
+	nextHandler Handler
 }
 
 func (sensitive *SensitiveHandler) Handle(content string) {
 	fmt.Println("执行敏感词过滤。。。")
 	newContent := strings.Replace(content, "敏感词", "***", 1)
 	fmt.Println(newContent)
-	sensitive.next(sensitive.handler, newContent)
+	sensitive.next(sensitive.nextHandler, newContent)
 }
 
 func (sensitive *SensitiveHandler) next(handler Handler, content string) {
-	if sensitive.handler != nil {
-		sensitive.handler.Handle(content)
+	if handler != nil {
+		handler.Handle(content)
 	}
 }
--- go-pattern/behavioral/chainofrspb_test.go
+++ go-pattern/behavioral/chainofrspb_test.go
@@ -7,7 +7,7 @@ func TestChain(t *testing.T) {
 	yellowHandler := &YellowHandler{}
 	sensitiveHandler := &SensitiveHandler{}
 	// 将责任链串起来
-	adHandler.handler = yellowHandler
-	yellowHandler.handler = sensitiveHandler
+	adHandler.nextHandler = yellowHandler
+	yellowHandler.nextHandler = sensitiveHandler
 	adHandler.Handle("我是正常内容，我是广告，我是涉黄，我是敏感词，我是正常内容")
 }
